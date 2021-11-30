ded20d8f958fbc76ece39aa682ddf0854d79d695
CodeInMyPC <thucnguyen3724@gmail.com>
2021-11-30 21:59:14 +0700

    third commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,14 @@ module.exports = {
     extend: {
       colors:{
         blue: {
-          111: "#ebf8ff"
+          111: "#ebf8ff",
+          112: '#3182ce'
+        },
+        gray:{
+          111: '#4a5568',
+          112: '#2d3748',
+          113: '#718096',
+          114: '#a0aec0',
         }
       }
     },
