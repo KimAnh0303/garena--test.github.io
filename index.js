27f8e229b04b273c68ab25378279dfd919789439
CodeInMyPC <thucnguyen3724@gmail.com>
2021-12-01 21:37:17 +0700

    thuc update slick slider 1

---
+++ index.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+  $('.list-slider').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    dots: true,
+    arrows:false,
+    responsive: [
+      {
+        breakpoint: 769,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
+});	
