3280b2ea19f21e6c0d7f201b1c71a53c94ddc0e1
KimAnh <lekimanh098@gmail.com>
2023-04-06 10:13:54 +0700

    upload

--- index.js
+++ index.js
@@ -1,48 +1,16 @@
 // slick slider
 $(document).ready(function(){
   $('.list-slider').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
     infinite: true,
-    slidesToShow: 3,
-    slidesToScroll: 3,
+    autoplay:true,
+    autoplaySpeed: 1200,
     dots: true,
-    arrows:false,
-    responsive: [
-      {
-        breakpoint: 769,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2
-        }
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1
-        }
-      }
-    ]
+    arrows:true,
+    prevArrow:
+    `<button type='button' class='slick-prev pull-left'><i class='fas fa-chevron-left'</i></button>`,
+    nextArrow:
+    `<button type='button' class='slick-next pull-right'><i class='fas fa-chevron-right'</i></button>`,
   });
 });	
-
-// navbar
-function scrollHeader(){
-  const nav = document.getElementById('header-wrap');
-  //When the scroll is greater than 200 viewport height, add the scroll-header class to the header tag
-  if(this.scrollY >= 100) nav.classList.add('scroll-header'); 
-  else nav.classList.remove('scroll-header');
-}
-
-window.addEventListener('scroll',scrollHeader);
-
-
-
-// =========================SHOW MENU========================
-0
-
-$(document).ready(function () {
-    $("#btn-navbar").click(function () {
-        $("#topnav").toggleClass("show-menu");
-        return false;
-    });
-});
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,24 +3,45 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
+      spacing: {
+        '30r': '30rem',
+        '25': '50px',
+        '250': '500px',
+        '282': '564px',
+        '363': '726px',
+        '124': '248px',
+        '135': '270px',
+        '160': '320px',
+        '173': '346px',
+
+
+      },
       colors:{
-        blue: {
-          111: "#ebf8ff",
-          112: '#3282ce'
-          
+        primary:{
+          1:'hsla(0,0%,45.9%,.85)',
+          5: '#f23737',
+          10:'#f04145',
         },
-        gray:{
-          111: '#4a5568',
-          112: '#2d3748',
-          113: '#718096',
-          114: '#a0aec0',
-        }
+        bgprm:{
+          5: '#f8f9fa',
+        },
+        bd:{
+          5:' #eee',
+        },
+        maxWidth: {
+          '270': '270px',
+
+        },
+        minHeight:{
+          '160': '320px',
+        },
       },
       screens:{
-        'down_lg': {'max':'1025px'},
-        'down_md': {'max':'769px'},
-        'down_sm': {'max':'480px'},
-      }
+        'lg': {'min':'1023px'},
+        'down_lg': {'max':'1023px' },
+        'md': {'min':'740px'},
+        'sm': {'max':'480px'},
+      },
     },
   },
 
