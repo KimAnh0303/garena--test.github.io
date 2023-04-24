5fa261615e2edbb8137f1bc88ab2890cba371ea4
KimAnh <lekimanh098@gmail.com>
2023-04-24 11:58:04 +0700

    upload slide

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
     slidesToShow: 1,
     slidesToScroll: 1,
     infinite: true,
-    autoplay:true,
+    // autoplay:true,
     autoplaySpeed: 1200,
     dots: true,
     arrows:true,
@@ -14,3 +14,16 @@ $(document).ready(function(){
     `<button type='button' class='slick-next pull-right'><i class='fas fa-chevron-right'</i></button>`,
   });
 });	
+$(document).ready(function(){
+  $('.slider-2').slick({
+  slidesToShow: 1,
+  centerMode: true,
+  centerPadding: "10%",
+  speed: 500,
+    prevArrow:
+    `<button type='button' class='slick-prev pull-left'><i class='fas fa-chevron-left'</i></button>`,
+    nextArrow:
+    `<button type='button' class='slick-next pull-right'><i class='fas fa-chevron-right'</i></button>`,
+  });
+});	
+
